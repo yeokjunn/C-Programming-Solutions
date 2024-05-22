5ade5b46a79cc9b3cccc8c360c556d0f71748cbc
Ke Jun <138819401+yeokjunn@users.noreply.github.com>
2024-05-23 00:04:51 +0800

    Add files via upload

---
+++ Graph-Solutions/Assignment5_Q1.c
@@ -0,0 +1,183 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _listnode
+{
+    int vertex;
+	struct _listnode *next;
+} ListNode;
+typedef ListNode StackNode;
+
+typedef struct _graph{
+    int V;
+    int E;
+    int *visited; //store the visited vertices
+    int **matrix;
+}Graph;
+
+typedef struct _linkedlist
+{
+	int size;
+	ListNode *head;
+} Stack;
+
+// You should not change the prototypes of these functions
+//////STACK///////////////////////////////////////////
+void push(Stack *sPtr, int vertex);
+int pop(Stack *sPtr);
+int peek(Stack s);
+int isEmptyStack(Stack s);
+void removeAllItemsFromStack(Stack *sPtr);
+////GRAPH////////////////////////////////////////////
+void printGraphMatrix(Graph );
+////////////////////////////////////////////
+
+int Connected (Graph);
+
+int main()
+{
+    Graph g;
+    int i,j;
+    
+
+    printf("Enter the number of vertices:\n");
+    scanf("%d",&g.V);
+
+    g.E = 0;
+    g.matrix = (int **)malloc(g.V*sizeof(int *));
+    for(i=0;i<g.V;i++)
+        g.matrix[i] = (int *)malloc(g.V*sizeof(int));
+
+    for(i=0;i<g.V;i++)
+       for(j=0;j<g.V;j++)
+           g.matrix[i][j] = 0;
+	
+    g.visited = (int *) malloc(sizeof(int)*g.V);
+    for(i=0;i<g.V;i++) 
+        g.visited[i] = 0;
+    
+    
+    int V1, V2;
+    printf("Enter two vertices which are adjacent to each other: (press a to stop)\n");
+    while(scanf("%d %d",&V1,&V2)==2)
+    {
+        if(V1>0 && V1<=g.V && V2>0 && V2<=g.V)
+        {
+            g.matrix[V1-1][V2-1] = 1;
+            g.matrix[V2-1][V1-1] = 1;
+            g.E++;
+        }
+        else
+            break;
+        printf("Enter two vertices which are adjacent to each other: (press a to stop)\n");
+    }
+    scanf("%*c");
+    
+    
+    int res = Connected(g);
+    if(res ==1)
+        printf("The graph is connected.\n");
+    else
+        printf("The graph is not connected.\n");
+
+    return 0;
+}
+
+int Connected (Graph g)
+{
+    // Initialisations
+    int i, j = 0;
+    int currentVertex, unvisitedIndicator;
+    Stack explorationStack;
+    explorationStack.head = NULL;
+    explorationStack.size = 0;
+
+    // Initialising nodes to not visited
+    for (i = 0; i < g.V; i++)
+    {
+        g.visited[i] = 0;
+    }
+
+    // Push & mark 1st node as visited
+    g.visited[0] = 1;
+    push(&explorationStack, g.visited[0]);
+
+    // Check that every vertex is connected to the other vertices, with DFS
+    while (!isEmptyStack(explorationStack))
+    {
+        currentVertex = peek(explorationStack);
+        unvisitedIndicator = 0;
+        for (i = 0; i < g.V; i++)
+        {
+            if (g.matrix[currentVertex-1][i] == 1 && g.visited[i] == 0)
+            {
+                g.visited[i] = 1;
+                push(&explorationStack, i+1);
+                unvisitedIndicator = 1;
+            }
+        }
+        if (unvisitedIndicator == 0)
+        {
+            pop(&explorationStack);
+        }
+    }
+    // Check for graph connectivity
+    for (j = 0; j < g.V; j++)
+    {
+        if (g.visited[j] == 0)
+        {
+            return -1; // Graph not connected
+        }   
+    }
+    return 1; //Graph Connected
+}
+void printGraphMatrix(Graph g)
+{
+    int i,j;
+
+    for(i=0;i<g.V;i++){
+        for(j=0;j<g.V;j++)
+            printf("%d\t",g.matrix[i][j]);
+        printf("\n");
+    }
+
+}
+
+void push(Stack *sPtr, int vertex)
+{
+	StackNode *newNode;
+    newNode= malloc(sizeof(StackNode));
+    newNode->vertex = vertex;
+    newNode->next = sPtr->head;
+    sPtr->head = newNode;
+    sPtr->size++;
+}
+
+int pop(Stack *sPtr)
+{
+    if(sPtr==NULL || sPtr->head==NULL){
+        return 0;
+    }
+    else{
+       StackNode *temp = sPtr->head;
+       sPtr->head = sPtr->head->next;
+       free(temp);
+       sPtr->size--;
+       return 1;
+    }
+}
+
+int isEmptyStack(Stack s)
+{
+     if(s.size==0) return 1;
+     else return 0;
+}
+
+int peek(Stack s){
+    return s.head->vertex;
+}
+
+void removeAllItemsFromStack(Stack *sPtr)
+{
+	while(pop(sPtr));
+}
---
+++ Graph-Solutions/Assignment5_Q3.c
@@ -0,0 +1,194 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _listnode
+{
+    int vertex;
+	struct _listnode *next;
+} ListNode;
+
+typedef struct _graph{
+    int V;
+    int E;
+    int *visited;
+    int **matrix;
+}Graph;
+
+typedef ListNode QueueNode;
+
+typedef struct _queue{
+   int size;
+   QueueNode *head;
+   QueueNode *tail;
+} Queue;
+
+int SD (Graph G, int v, int w);
+
+void printGraphMatrix(Graph );
+
+// You should not change the prototypes of these functions
+void enqueue(Queue *qPtr, int item);
+int dequeue(Queue *qPtr);
+int getFront(Queue q);
+int isEmptyQueue(Queue q);
+void removeAllItemsFromQueue(Queue *qPtr);
+
+int main()
+{
+    Graph g;
+    int i,j;
+
+    printf("Enter the number of vertices:\n");
+    scanf("%d",&g.V);
+
+    g.E = 0;
+    g.matrix = (int **)malloc(g.V*sizeof(int *));
+    for(i=0;i<g.V;i++)
+        g.matrix[i] = (int *)malloc(g.V*sizeof(int));
+
+    for(i=0;i<g.V;i++)
+       for(j=0;j<g.V;j++)
+           g.matrix[i][j] = 0;
+
+    g.visited = (int *) malloc(sizeof(int)*g.V);
+    for(i=0;i<g.V;i++) g.visited[i] = 0;
+
+    int V1, V2;
+    printf("Enter two vertices which are adjacent to each other:\n");
+    while(scanf("%d %d",&V1,&V2)==2)
+    {
+        if(V1>0 && V1<=g.V && V2>0 && V2<=g.V)
+        {
+            g.matrix[V1-1][V2-1] = 1;
+            g.matrix[V2-1][V1-1] = 1;
+            g.E++;
+        }
+        else
+            break;
+        printf("Enter two vertices which are adjacent to each other: (press a to stop)\n");
+    }
+    scanf("%*c");
+    // printGraphMatrix(g);
+    
+    printf("Enter two vertices for finding their shortest distance: (press a to stop)\n");
+    scanf("%d %d", &i, &j);
+
+    int d = SD(g,i,j);
+    if(d==-1)
+        printf("%d and %d are unconnected.\n",i,j);
+    else
+        printf("The shortest distance is %d\n",d);
+    return 0;
+}
+
+/*Initialization: Enqueue the starting node into a queue and mark it as visited.
+Exploration: While the queue is not empty:
+Dequeue a node from the queue and visit it (e.g., print its value).
+For each unvisited neighbor of the dequeued node:
+Enqueue the neighbor into the queue.
+Mark the neighbor as visited.*/
+// To access adjacent nodes - use g.matrix[node1][node2]
+
+int SD(Graph g, int v, int z){
+    // Initialisations
+    int distance = 0;
+    int i, j, breadth = 0;
+    int currentVertex; 
+    Queue explorationQueue;
+    explorationQueue.head, explorationQueue.tail = NULL;
+    explorationQueue.size = 0;
+
+    // Initialise *visited of all nodes to be 0
+    for (i = 0; i < g.V; i++)
+    {
+        g.visited[i] = 0;
+    }
+
+    // Enqueue starting node, mark as visited
+    enqueue(&explorationQueue, v);
+    g.visited[v-1] = 1;
+    
+    while (!isEmptyQueue(explorationQueue))
+    {
+        breadth = explorationQueue.size;
+        distance++; 
+        for (j = 0; j < breadth; j++)
+        {
+            currentVertex = dequeue(&explorationQueue);
+            for (i = 0; i < g.V; i++)
+            {
+                if (g.matrix[currentVertex-1][i] == 1 && g.visited[i] == 0)
+                {
+                    g.visited[i] = 1;
+                    enqueue(&explorationQueue, i + 1);
+                    if (i+1 == z)
+                    {
+                        return distance;
+                    }
+                }
+            }
+        }
+    }
+    return -1;
+}
+
+void printGraphMatrix(Graph g)
+{
+    int i,j;
+
+    for(i=0;i<g.V;i++){
+        for(j=0;j<g.V;j++)
+            printf("%d\t",g.matrix[i][j]);
+        printf("\n");
+    }
+
+}
+
+
+void enqueue(Queue *qPtr, int item) {
+    QueueNode *newNode;
+    newNode = malloc(sizeof(QueueNode));
+    if(newNode==NULL) exit(0);
+
+    newNode->vertex = item;
+    newNode->next = NULL;
+
+    if(isEmptyQueue(*qPtr))
+        qPtr->head=newNode;
+    else
+        qPtr->tail->next = newNode;
+
+    qPtr->tail = newNode;
+    qPtr->size++;
+}
+
+int dequeue(Queue *qPtr) {
+    if(qPtr==NULL || qPtr->head==NULL){ //Queue is empty or NULL pointer
+        return 0;
+    }
+    else{
+       int headValue = qPtr->head->vertex;
+       QueueNode *temp = qPtr->head;
+       qPtr->head = qPtr->head->next;
+       if(qPtr->head == NULL) //Queue is emptied
+           qPtr->tail = NULL;
+
+       free(temp);
+       qPtr->size--;
+       return headValue;
+    }
+}
+
+int getFront(Queue q){
+    return q.head->vertex;
+}
+
+int isEmptyQueue(Queue q) {
+    if(q.size==0) return 1;
+    else return 0;
+}
+
+void removeAllItemsFromQueue(Queue *qPtr)
+{
+	while(dequeue(qPtr));
+}
